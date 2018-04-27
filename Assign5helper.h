0b8874e00f8feda966cbff570f79f8c1e4d3c418
ak-iitjm <31209275+ak-iitjm@users.noreply.github.com>
2018-04-27 20:34:11 +0530

    Created a kind of basic template

--- Assign5helper.h
+++ Assign5helper.h
@@ -1,12 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <map>
+#include <fstream>
+#include <iomanip>
 using namespace std;
 
 vector<string> instructions;
 map<string,int> labels;
 map<string,int> latency;
+bool pEnd = false;
 
+bool valid(string s)                    // helper function in determining operand for register
+{
+    if(s.length()>8) return false;
+    for(int i = 1;i<s.length();i++)
+    {
+        if(s[i]<'0'||s[i]>'9') return false;
+    }
+    return true;
+}
+
+
+void forward()
+{
+	cout<<"forwarded\n";
+}
+void writeback()
+{
+	cout<<"Oye Mundea\n";
+}
+void memoryAccess()
+{
+	cout<<"Memory Chalao\n";
+}
+void execute()
+{
+	cout<<"Execution Done\n";
+}
+void idecode()
+{
+	cout<<"Hadippa\n";
+}
+void ifetch()
+{
+	cout<<"Burrah\n";
+}
 
 void nextCycle()
 {
-	
+	forward();
+	writeback();
+	memoryAccess();
+	execute();
+	idecode();
+	ifetch();
 }
--- main.cpp
+++ main.cpp
@@ -1,11 +1,101 @@
-#include <iostream>
+#include "Assign5helper.h"
 using namespace std;
-int r[16],o1,o2,o3;
-bool end = false,i = 3;
-int main()
+int r[13];
+int main (int argc,char *argv[]) 
 {
-	cout<<"Welcome to ARM-Sim(Pipelined Version)\n";
+    ifstream lfile(argv[2]);
+    string line,num,eq;    
+    pair<string,int> label1;
+    if(argc>2)
+    {
+        if(lfile.is_open())
+        {
+            while ( lfile>>line )
+            {
+                if(line[0]=='#')
+                {
+                    string leaveline;
+                    getline(lfile,leaveline);
+                    continue;
+                }
+                for(int i=0;i<line.length();i++)
+                {
+                    if(line[i]>='A'&&line[i]<='Z')
+                    {
+                        line[i] = line[i] - 'A' + 'a';
+                    }
+                }
+                lfile>>eq;
+                if(eq=="=")
+                {
+                    lfile>>num;
+                    if(num[num.length()-1]==';') num = num.substr(0,num.length()-1);
+                    if(!valid(num))
+                    {
+                        cout<<"Latency of "<<line<<"not Valid in latency file\n";
+                        return 0;
+                    }
+                }
+                else
+                {
+                    cout<<"Error in latency File\n";
+                    return 0;
+                }
+                latency[line] = stoi(num);
+            }  
+        }
+    }
+    ifstream myfile(argv[1]);
+    if (myfile.is_open())
+    {
+        while ( myfile>>line )
+        {
+            if(line==",") continue;
+            if(line[line.length()-1]==',') line = line.substr(0,line.length()-1);
+            for(int i=0;i<line.length();i++)
+            {
+                if(line[i]>='A'&&line[i]<='Z')
+                {
+                    line[i] = line[i] - 'A' + 'a';
+                }
+            }
+            if(line[line.length() - 1]==':')
+            {
+                label1.second = instructions.size();
+                label1.first = line.substr(0,line.length()-1);
+                labels[label1.first] = label1.second;
+                continue;
+            }
+            if(line[0]=='@'){
+                string leaveline;
+                getline(myfile,leaveline);
+                continue;
+            }
+            instructions.push_back(line);
+        }
+
+        myfile.close();
+    }
+    else
+    {
+    	cout << "Unable to open file\n";
+    	return 0;
+    }
+    cout<<"Welcome to ARM-Sim(Pipelined Version)\n";
 	cout<<"The program operates in different modes.\n";
-	cout<<"1). For step by step execution, type\'s\' \n2). For full execution at any stage, press\'f\'(default mode).";
-	while(!end)
+	cout<<"1). For step by step execution, type\'s\' \n2). For full execution at any stage, press\'f\'(default mode).\n\n";
+	char mode = 'k';
+	while(!pEnd)
+	{
+		if(mode!='f')
+		{
+			cout<<"Mode : ";
+			cin>>mode;
+			cout<<mode<<endl;
+			if(mode !='s') mode = 'f';
+		}
+		else mode = 'f';
+		nextCycle();
+	}
+    return 0;
 }
